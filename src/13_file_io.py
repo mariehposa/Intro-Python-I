d22a7295aae91d37ea81cd23b528cdf145749fcb
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-02-12 08:31:27 +0100

    complete file

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('foo.txt') as f:
+    read_data = f.read()
+    print(read_data)  
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open ('bar.txt', "w+") as m:
+    checkData = m.write("I have no idea what to write here.\nMaybe i should think.\nI have an idea.")
+print(m.closed)
+with open('bar.txt') as b:
+    myData = f.read()
+    print(myData)
